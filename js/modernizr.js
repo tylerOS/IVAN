38da61f115df03304dea29d9c9ecb8a690827214
Tyler <ty.tyler.dl@gmail.com>
2016-09-07 10:07:46 -0700

    Corrected last upload. Minor text fix

--- js/modernizr.js
+++ js/modernizr.js
@@ -1,5 +1,5 @@
 /*!
- * Modernizr v2.8.3
+ * Modernizr v2.7.1
  * www.modernizr.com
  *
  * Copyright (c) Faruk Ates, Paul Irish, Alex Sexton
@@ -24,7 +24,7 @@
 
 window.Modernizr = (function( window, document, undefined ) {
 
-    var version = '2.8.3',
+    var version = '2.7.1',
 
     Modernizr = {},
 
@@ -156,7 +156,7 @@ window.Modernizr = (function( window, document, undefined ) {
 
       var matchMedia = window.matchMedia || window.msMatchMedia;
       if ( matchMedia ) {
-        return matchMedia(mq) && matchMedia(mq).matches || false;
+        return matchMedia(mq).matches;
       }
 
       var bool;
@@ -605,7 +605,7 @@ window.Modernizr = (function( window, document, undefined ) {
 
     // Note, Android < 4 will pass this test, but can only animate
     //   a single property at a time
-    //   goo.gl/v3V4Gp
+    //   daneden.me/2011/12/putting-up-with-androids-bullshit/
     tests['cssanimations'] = function() {
         return testPropsAll('animationName');
     };
